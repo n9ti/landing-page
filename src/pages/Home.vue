f32cc5dab7ffb689f966b945196369c3118aa465
Nati <nati.n@linecorp.com>
2020-04-15 03:57:48 +0700

    fix button

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -12,10 +12,10 @@
     <br />
     <img src="https://linemyshop.com/images/Asset-Hero.svg" alt width="320px" />
     <p>
-      <em id="register-2" href="/thank">
-        <b>Register &rarr; Direct</b>
-      </em>
-      <a id="register-1" href="https://ga-app.netlify.com">
+      <a id="register-direct" href="/thank">
+        <em>Register &rarr; Direct</em>
+      </a>
+      <a id="register-undirect" href="https://ga-app.netlify.com">
         <b>Register &rarr;</b>
       </a>
     </p>
