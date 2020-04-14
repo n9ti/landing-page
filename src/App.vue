e8385e1aa1b8a6874a9668b7c51ce9963c811013
Nati Namvong <nati@flyingcomma.com>
2020-04-15 00:24:36 +0700

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <header>
+      <h1>
+        Page Heading with
+        <i>Italics</i> and
+        <u>Underline</u>
+      </h1>
+      <p>
+        Page Subheading with
+        <mark>highlighting</mark>
+      </p>
+      <br>
+      <img src="https://linemyshop.com/images/Asset-Hero.svg" alt width="320px">
+      <p>
+        <a href="https://d3lpn.csb.app">
+          <b>Register &rarr;</b>
+        </a>
+      </p>
+    </header>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App"
+};
+</script>
+
+<style>
+</style>
